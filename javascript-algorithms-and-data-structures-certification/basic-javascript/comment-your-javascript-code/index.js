522933cffffeac02bb21b20caceb230a963847da
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-01-26 07:36:58 -0500

    Added 1 FCC solution - basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/comment-your-javascript-code/index.js
@@ -0,0 +1,27 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Tue 26 Jan 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Responsive Web Design Certification - CSS Grid  
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: CSS Grid
+	- Task: <fill-this-in>
+*/
+
+// a single line comment 
+
+/* 
+	a multi-line coment
+*/ 
+
+console.log("Example of single and mulit-line comments in javascript");
+
+// EOF
