7b476c6d94853f24cd3109a5ced43342ef4f5c95
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-01-28 06:58:38 -0500

    Added 1 FCC solution - Basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/storing-values-with-the-assignment-operator/index.js
@@ -0,0 +1,29 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Thu 28 Jan 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms Certification - Basic Javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: Basic Javascript 
+	- Task: Storing Values with the Assignment Operator
+*/
+
+console.log("FCC - Basic Javascript - Storing Values with the Assignment Operator");
+
+// Setup
+var a;
+
+// Only change code below this line
+a = 7;
+
+console.log("Value in variable a is: " + a);
+
+// EOF
