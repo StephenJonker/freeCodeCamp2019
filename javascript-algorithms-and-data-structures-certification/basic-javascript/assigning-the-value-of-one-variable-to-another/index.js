34e74645bc27df082eb2cfbb1576671acec98420
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-01-29 06:48:59 -0500

    Added 1 FCC solution - basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/assigning-the-value-of-one-variable-to-another/index.js
@@ -0,0 +1,33 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Fri 29 Jan 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - javascript and algorithms - basic javascript 
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Assigning the Value of One Variable to Another
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+// Assign the contents of a to variable b.
+b = a;
+
+console.log("Value of b is: " + b);
+
+// EOF
