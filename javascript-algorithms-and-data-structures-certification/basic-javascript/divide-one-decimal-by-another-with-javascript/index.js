d127ff489370f80554505f6f8503d2549382096d
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-01-31 10:39:02 -0500

    Added 1 FCC solution - basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/divide-one-decimal-by-another-with-javascript/index.js
@@ -0,0 +1,29 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Sat 30 Jan 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - basic javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Divide One Decimal by Another with JavaScript 
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Goal: 
+// Change the 0.0 so that quotient will equal to 2.2.
+// var quotient = 0.0 / 2.0; // Change this line
+
+var quotient = 4.4 / 2.0; // Change this line 
+
+console.log("The value in variable quotient is: " + quotient + " Expect 2.2");
+
+// EOF
