e758d4c8d52c5056840e3c5019a72ee21439dbce
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-02-01 17:09:45 -0500

    Added 2 FCC solutions - basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/concatenating-strings-with-the-plus-equals-operator/index.js
@@ -0,0 +1,35 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Sun 31 Jan 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - basic javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Concatenating Strings with the Plus Equals Operator 
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Goal: 
+// Build myStr over several lines by concatenating these two strings: 
+// "This is the first sentence. " and "This is the second sentence." 
+// using the += operator. Use the += operator similar to how it is 
+// shown in the editor. Start by assigning the first string to myStr, 
+// then add on the second string.
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
+console.log("The value in variable myStr is: " + myStr);
+
+// EOF
---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/constructing-strings-with-variables/index.js
@@ -0,0 +1,30 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Mon 01 Feb 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - basic javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Constructing Strings with Variables 
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Goal:
+// Set myName to a string equal to your name and build myStr with 
+// myName between the strings "My name is " and " and I am well!"
+// Only change code below this line
+var myName = "James Bond";
+var myStr = "My name is " + myName + " and I am well!"; 
+
+console.log("The value in variable myStr is: " + myStr);
+
+// EOF
