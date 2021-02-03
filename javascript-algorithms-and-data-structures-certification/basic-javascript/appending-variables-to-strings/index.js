637b92141b74b8dd9a8d3831a670d76b26b903ce
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-02-03 06:47:19 -0500

    Added 1 FCC solution - basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/appending-variables-to-strings/index.js
@@ -0,0 +1,32 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Wed 3 Jan 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - basic javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Appending Variables to Strings 
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Goal: 
+// Define a variable a with var and initialize it to a value of 9.
+
+// Change code below this line
+
+var someAdjective = "Great!";
+var myStr = "Learning to code is ";
+myStr += someAdjective; 
+
+console.log("The value in variable myStr is: " + myStr);
+
+// EOF
