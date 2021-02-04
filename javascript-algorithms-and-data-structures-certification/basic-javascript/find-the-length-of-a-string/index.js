ce4cb5af8f807b4a13b5c44152d9459b88896941
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-02-04 07:05:13 -0500

    Added 1 FCC solution - Basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/find-the-length-of-a-string/index.js
@@ -0,0 +1,36 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Thu 04 Jan 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - basic javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Find the Length of a String 
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Goal: 
+// Use the .length property to count the number of 
+// characters in the lastName variable and assign it to lastNameLength.
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
+
+console.log("The value in variable lastName is: " + lastName);
+console.log("The length of variable lastName is: " + lastNameLength);
+
+// EOF
