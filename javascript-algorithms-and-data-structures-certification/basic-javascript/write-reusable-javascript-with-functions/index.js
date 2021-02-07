ab534ce002b6387a744cd780aa319c6516ea6c2b
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-02-07 06:44:49 -0500

    Added 1 FCC solution - Basic Javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/write-reusable-javascript-with-functions/index.js
@@ -0,0 +1,32 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Sun 07 Feb 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - basic javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Write Reusable JavaScript with Functions 
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Goal: 
+// Create a function called reusableFunction which prints "Hi World" to the dev console.
+// Call the function.
+
+function reusableFunction() {
+    console.log("Hi World");
+}
+
+reusableFunction();
+
+
+// EOF
