a4ed44e76449a738d1ee14da1e840710555b0afc
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-02-08 16:39:38 -0500

    Added 1 FCC solution - basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/passing-values-to-functions-with-arguments/index.js
@@ -0,0 +1,33 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Mon 08 Feb 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - basic javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Passing Values to Functions with Arguments
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Goal: 
+// Create a function called functionWithArgs that accepts two arguments and 
+// outputs their sum to the dev console. Call the function with two numbers as arguments.
+
+function functionWithArgs(parm1, parm2) {
+    console.log(parm1 + parm2);
+}
+
+functionWithArgs(1,2); 
+
+console.log("The result should be 3 in line above");
+
+// EOF
