ceb2372fd2fa4c2cd322d0b35e150f453a52affd
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-02-09 16:14:32 -0500

    Added 1 FCC solution - basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/return-a-value-from-a-function-with-return/index.js
@@ -0,0 +1,34 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Tue 09 Feb 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - basic javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Return a Value from a Function with Return 
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Goal: 
+// Create a function timesFive that accepts one argument, multiplies 
+// it by 5, and returns the new value. See the last line in the editor 
+// for an example of how you can test your timesFive function.
+
+function timesFive(arg1) {
+	return arg1 * 5;
+}
+
+var result = timesFive(3);
+
+console.log("The value in variable resutl is: " + result + " Expect value of 15");
+
+// EOF
