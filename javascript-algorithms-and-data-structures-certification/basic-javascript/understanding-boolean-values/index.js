110f0a25985826bc63662d8e0921b5e465b3d37a
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-02-13 07:23:59 -0500

    Added 1 FCC soltuion - basic javascript

---
+++ javascript-algorithms-and-data-structures-certification/basic-javascript/understanding-boolean-values/index.js
@@ -0,0 +1,41 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Sat 13 Feb 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - basic javascript   
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: basic javascript 
+	- Task: Understanding Boolean Values 
+*/
+
+console.log("FCC - Javascript and Algorithms - basic javascript");
+
+// Goal: 
+// Modify the welcomeToBooleans function so that it 
+// returns true instead of false when the run button is clicked.
+
+function welcomeToBooleans() {
+
+	// Only change code below this line
+  
+	return true; // Change this line
+  
+	// Only change code above this line
+  } 
+
+if ( welcomeToBooleans() == true ) {
+	console.log("function welcomToBooleans returned TRUE");
+} else {
+	console.log("function welcomToBooleans returned FALSE");
+}
+
+
+// EOF
