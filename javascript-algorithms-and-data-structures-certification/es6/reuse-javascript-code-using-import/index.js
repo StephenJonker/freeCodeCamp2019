bddb331829f72fd65f42777cb7df487859010a7f
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-03-07 13:54:23 -0500

    checking in intermediate code for import example code in es6

---
+++ javascript-algorithms-and-data-structures-certification/es6/reuse-javascript-code-using-import/index.js
@@ -0,0 +1,29 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Sun 7 Mar 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - es6  
+	
+	- This code is based on a template created by me to provide basic HTML structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: es6
+	- Task: Reuse JavaScript Code Using import
+*/
+
+console.log("FCC - Javascript and Algorithms - es6");
+
+// Goal: 
+// Reuse JavaScript Code Using import
+
+// New code goes here
+
+let res = "<tbd>"; 
+console.log("The value in variable res is: " + res + " Expect <tbd> ");
+
+// EOF
---
+++ javascript-algorithms-and-data-structures-certification/es6/reuse-javascript-code-using-import/myModule.js
@@ -0,0 +1,11 @@
+//
+// Javascript module with add function for use in html file
+// 
+
+function toUpper(string) {
+    return string.toUpperCase();
+}
+
+export { toUpper }; 
+
+//
