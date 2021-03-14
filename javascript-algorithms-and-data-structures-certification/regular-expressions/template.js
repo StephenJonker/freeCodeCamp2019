495df9f6f09ce2b8490a09fc566f99f79388c9ba
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-03-14 16:57:40 -0400

    Setup regular expressions, setup.sh and template file for Regex section for FCC

---
+++ javascript-algorithms-and-data-structures-certification/regular-expressions/template.js
@@ -0,0 +1,29 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Sun 14 Mar 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - regular expressions  
+	
+	- This code is based on a template created by me to provide basic script structure
+	- Additional template code provided by freeCodeCamp
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: regular expressions
+	- Task: <tbd>
+*/
+
+console.log("FCC - Javascript and Algorithms - regular expressions");
+
+// Goal: 
+// <tbd>
+
+// New code goes here
+
+let res = "<tbd>"; 
+console.log("The value in variable res is: " + res + " Expect <tbd> ");
+
+// EOF
