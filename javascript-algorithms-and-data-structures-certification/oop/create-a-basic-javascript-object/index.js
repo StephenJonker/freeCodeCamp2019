cba9c0bc64cfaf9e4a9dd7886d1dbf0cb4d91a78
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-04-26 17:27:51 -0400

    Added 1 FCC solution - OOP - and setup and templetes for new section

---
+++ javascript-algorithms-and-data-structures-certification/oop/create-a-basic-javascript-object/index.js
@@ -0,0 +1,33 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Mon 26 Apr 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - Object Oriented Programming
+	
+	- This code is based on a template created by me to provide basic script structure
+	- Additional template code provided by freeCodeCamp - (www.freecodecamp.com)
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: Object Oriented Programming
+	- Task: Create a Basic JavaScript Object
+*/
+
+console.log("FCC - Javascript and Algorithms - Object Oriented Programming");
+
+// Goal: 
+// Create a dog object with name and numLegs properties, and set them to a 
+// string and a number, respectively.
+
+let dog = {
+	"name": "Rover",
+	"numLegs": 4
+};
+
+
+console.log("The value in variable dog is: " + JSON.stringify(dog) + " Expect {\"name\":\"Rover\",\"numLegs\":4} ");
+
+// EOF
