6a3f9c6fd349ec9d415d955bae310ff3d57b75a7
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-04-27 06:54:37 -0400

    Added 1 FCC solution - OOP - display object properties with dot notication

---
+++ javascript-algorithms-and-data-structures-certification/oop/use-dot-notation-to-access-the-properties-of-an-object/index.js
@@ -0,0 +1,34 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Tue 27 Apr 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - Object Oriented Programming
+	
+	- This code is based on a template created by me to provide basic script structure
+	- Additional template code provided by freeCodeCamp - (www.freecodecamp.com)
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: Object Oriented Programming
+	- Task: Use Dot Notation to Access the Properties of an Object
+
+*/
+
+console.log("FCC - Javascript and Algorithms - Object Oriented Programming");
+
+// Goal: 
+// Print both properties of the dog object to your console.
+
+let dog = {
+	name: "Spot",
+	numLegs: 4
+};
+// Only change code below this line
+
+console.log(dog.name);
+console.log(dog.numLegs);
+
+// EOF
