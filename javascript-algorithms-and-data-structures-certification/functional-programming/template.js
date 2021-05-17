2acb0089a7d54d9b962f8564809181be144f7c72
Stephen Jonker <stephen.jonker.ca@gmail.com>
2021-05-17 13:21:25 -0400

    setup for new section - functional programming

---
+++ javascript-algorithms-and-data-structures-certification/functional-programming/template.js
@@ -0,0 +1,29 @@
+/*
+	Javascript file
+
+	Written by: Stephen Jonker
+	Written on: Mon 17 May 2021
+	Copyright (c) 2021 - Stephen Jonker - www.stephenjonker.com
+	
+	Project: freeCodeCamp - Javascript and Algorithms - Functional Programming
+	
+	- This code is based on a template created by me to provide basic script structure
+	- Additional template code provided by freeCodeCamp - (www.freecodecamp.com)
+	- Additions made by me to make it work
+	
+	Goal: 
+	- Section: Functional Programming
+	- Task: <tbd>
+*/
+
+console.log("FCC - Javascript and Algorithms - Functional Programming");
+
+// Goal: 
+// <tbd>
+
+// New code goes here
+
+let result = "<tbd>"; 
+console.log("The value in variable result is: " + result + " Expect <tbd> ");
+
+// EOF
